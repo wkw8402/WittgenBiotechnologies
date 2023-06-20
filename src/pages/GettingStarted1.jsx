4d4a351c8c0b9afc8193074f6e52a1876769ab98
franklee0810 <franklee0180@gmail.com>
2023-06-20 01:03:55 -0700

    수정

--- src/pages/GettingStarted1.jsx
+++ src/pages/GettingStarted1.jsx
@@ -31,6 +31,14 @@ export default function GettingStarted1() {
       {/*<meta name=description content="This site was generated with Anima. www.animaapp.com"/>*/}
       {/* <link rel="shortcut icon" type=image/png href="https://animaproject.s3.amazonaws.com/home/favicon.png" /> */}
       <meta name="viewport" content="width=1920, maximum-scale=1.0" />
+
+      {/* Oxygen font google link */}
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@600&family=Open+Sans:ital,wght@0,400;0,700;1,700;1,800&family=Oxygen:wght@300;400;700&display=swap"
+        rel="stylesheet"
+      />
       <link
         rel="shortcut icon"
         type="image/png"
@@ -48,10 +56,10 @@ export default function GettingStarted1() {
         defaultValue="apply-getting-started-1"
       />
 
-      <div className="flex flex-row relative bg-[FFFFFF] w-screen h-screen">
+      <div className="font-inter flex flex-row relative bg-[FFFFFF] w-screen h-screen relative">
         {/* LEFT SIDE BAR */}
         <div className="top-0 left-0 h-screen text-white bg-fixed bg-witgen w-195">
-          <div className="flex flex-row w-138 h-27 left-30.75 text-wg top-16.5 font-oxygen pt-16.5 pl-30.75 pr-26.25">
+          <div className="flex flex-row left-30.75 text-wg font-oxygen pt-16.5 pl-30.75 pr-26.25">
             <h1 className="font-bold">WittGen</h1>
             <h1 className="font-extralight">Portal</h1>
           </div>
@@ -109,40 +117,48 @@ export default function GettingStarted1() {
         </div>
 
         {/* header */}
-        <div className="absolute mt-[0px] ml-[193.5px] ">
+        <div className="absolute ml-[194.5px] border-b border-[#E9EBED] border-solid ">
           {/* inside of header */}
-          <div className="flex flex-row  items-center justify-center mt-[36px] ml-[90.5px]">
-            <ul className="flex flex-row gap-[48.5px] flex-nowrap">
-              <div className="flex flex-col">
+          <div className="flex flex-row items-center justify-center ">
+            <ul className="pt-[36px] pl-[60.5px] pb-[33px] flex flex-row gap-[48.5px] flex-nowrap ">
+              <div className="flex flex-col items-center justify-center">
                 <li className="whitespace-nowrap">Getting started</li>
-                <div className="w-[150px] h-[3px] bg-[#25474F]" />
+                <div className="w-[150px] h-[3px] bg-[#25474F] mt-[6px]" />
               </div>
-              <div className="flex flex-col">
+              <div className="flex flex-col items-center justify-center">
                 <li className="whitespace-nowrap leading-[16px]">
                   Databetabase Input
                 </li>
                 <div className="w-[150px] h-[3px] bg-[#F2F6F7]" />
               </div>
-              <div className="flex flex-col">
+              <div className="flex flex-col items-center justify-center">
                 <li className="whitespace-nowrap">Database Input</li>
                 <div className="w-[150px] h-[3px] bg-[#F2F6F7]" />
               </div>
-              <div className="flex flex-col">
+              <div className="flex flex-col items-center justify-center">
                 <li className="whitespace-nowrap">Submit</li>
                 <div className="w-[150px] h-[3px] bg-[#F2F6F7]" />
               </div>
             </ul>
-            <div className="flex flex-row whitespace-nowrap">
-              <button>Discard & Exit</button>
-              <button>Save & Exit</button>
+            <div className="flex flex-row gap-[7.5px] py-[33px] pr-[60px] ml-[130px]">
+              <button className=" border border-solid w-[120px] h-[30px] ">
+                <span className="font-semibold text-[10.5px] leading-[13px] ">
+                  Discard & Exit
+                </span>
+              </button>
+              <button className=" bg-[#25474F]    w-[120px] h-[30px] border border-solid border-gray-200">
+                <span className="text-white font-semibold text-[10.5px] leading-[13px] ">
+                  Save & Exit
+                </span>
+              </button>
             </div>
           </div>
         </div>
 
         {/* main */}
-        <div className="relative flex flex-row gap-[230.5px] mt-[126px]">
+        <div className="flex flex-row gap-[230.5px] mt-[126px]">
           {/* Left */}
-          <div className="flex flex-col justify-center item-center">
+          <div className="absolute flex flex-col justify-center item-center left-[253.6px] top-[126px] ">
             <form className="flex flex-col ml-[60px] gap-[12px]">
               <div className="flex flex-row gap-[12px] ">
                 <div>
@@ -153,9 +169,10 @@ export default function GettingStarted1() {
                     Application type
                   </label>
                   <input
-                    className="border justify-center p-[9px] w-[140px] h-[36px] "
+                    className="border justify-center p-[9px] w-[140px] h-[36px] bg-[#F1F3F4]  text-[14px]"
                     type="text"
                     id="company_name"
+                    placeholder="Researchers"
                   />
                 </div>
 
@@ -167,7 +184,7 @@ export default function GettingStarted1() {
                     Application ID
                   </label>
                   <input
-                    className="border justify-center p-[9px] w-[210px] h-[36px] "
+                    className="border justify-center p-[9px] w-[210px] h-[36px]  text-[14px]"
                     type="text"
                     id="department/laboratory"
                     placeholder="GH-13728930_1"
@@ -175,11 +192,13 @@ export default function GettingStarted1() {
                 </div>
               </div>
 
-              <div className="flex flex-row gap-[232.5px] mt-[30px]">
-                <h1 className="">Client Information</h1>
-                <div className="flex flex-row gap-[6px]">
+              <div className="flex flex-row gap-[170.5px] mt-[30px]">
+                <h1 className="text-[#25474F] font-semibold leading-[24px] text-[16px] ">
+                  Client Information
+                </h1>
+                <div className="flex flex-row">
                   <input type="checkbox" className="form-checkbox" />
-                  <h1 className="font-normal text-[14px] leading-[21px] text-[#464646] ">
+                  <h1 className="font-normal text-[14px] leading-[21px] text-[#464646] ml-[8px]">
                     Same as profile
                   </h1>
                 </div>
@@ -195,7 +214,7 @@ export default function GettingStarted1() {
                     First name
                   </label>
                   <input
-                    className="border justify-center p-[9px] w-[210px] h-[36px] "
+                    className="border justify-center w-[210px] h-[36px] text-[14px] "
                     type="text"
                     id="first_name"
                   />
@@ -209,7 +228,7 @@ export default function GettingStarted1() {
                     Last name
                   </label>
                   <input
-                    className="border justify-center p-[9px] w-[210px] h-[36px] "
+                    className="border justify-center w-[210px] h-[36px]  text-[14px]"
                     type="text"
                     id="last_name"
                   />
@@ -227,9 +246,10 @@ export default function GettingStarted1() {
                   </span>
                 </label>
                 <input
-                  className="border-solid border-[0.9px] border-[#CEDDE1]-400 justify-center pt-[9px] pb-[4.5px] pl-[9px] pr-[9px] w-[432px] h-[34px]"
+                  className="border-solid border-[0.9px] border-[#CEDDE1]-400 justify-center pt-[9px] pb-[4.5px] pl-[9px] pr-[9px] w-[432px] h-[34px]  text-[14px]"
                   type="email"
                   id="notification"
+                  placeholder="example@email.com"
                 />
               </div>
 
@@ -242,7 +262,7 @@ export default function GettingStarted1() {
                     Company name
                   </label>
                   <input
-                    className="border justify-center p-[9px] w-[210px] h-[36px] "
+                    className="border justify-center p-[9px] w-[210px] h-[36px] text-[14px] "
                     type="text"
                     id="company_name"
                   />
@@ -256,9 +276,10 @@ export default function GettingStarted1() {
                     Country
                   </label>
                   <input
-                    className="border justify-center p-[9px] w-[210px] h-[36px] "
+                    className="border justify-center p-[9px] w-[210px] h-[36px]  text-[14px]"
                     type="text"
                     id="country"
+                    placeholder="Select a country"
                   />
                 </div>
               </div>
@@ -270,7 +291,7 @@ export default function GettingStarted1() {
                   Company address
                 </label>
                 <input
-                  className="border-solid border-[0.9px] border-[#CEDDE1]-400 justify-center pt-[9px] pb-[4.5px] pl-[9px] pr-[9px] w-[432px] h-[34px]"
+                  className="border-solid border-[0.9px] border-[#CEDDE1]-400 justify-center pt-[9px] pb-[4.5px] pl-[9px] pr-[9px] w-[432px] h-[34px]  text-[14px]"
                   type="email"
                   id="notification"
                 />
@@ -281,12 +302,13 @@ export default function GettingStarted1() {
                     className="block font-normal text-[10.5px] leading-[16px] text-[#717F8C]"
                     htmlFor="State"
                   >
-                    State / privince
+                    State / province
                   </label>
                   <input
-                    className="border justify-center p-[9px] w-[210px] h-[36px] "
+                    className="border justify-center p-[9px] w-[210px] h-[36px]  text-[14px]"
                     type="text"
                     id="State"
+                    placeholder="Select a state/province"
                   />
                 </div>
 
@@ -298,9 +320,10 @@ export default function GettingStarted1() {
                     City
                   </label>
                   <input
-                    className="border justify-center p-[9px] w-[210px] h-[36px] "
+                    className="border justify-center p-[9px] w-[210px] h-[36px] text-[14px] "
                     type="text"
                     id="city"
+                    placeholder="Select a city"
                   />
                 </div>
               </div>
@@ -318,44 +341,50 @@ export default function GettingStarted1() {
                 />
               </div>
             </form>
-
-            <button className="mt-[119.5px] ml-[60px] justify-center items-center  w-[135px] h-[36px] border border-solid border-gray-200">
-              <span className="font-normal text-[10.5px] py-[18px] px-[60px] leading-[13px] text-[#25474F]">
-                Back
-              </span>
-            </button>
           </div>
 
           {/* Right Part */}
-          <div className="w-[463.5px] h-[546px] flex flex-col gap-[18px]">
+          <div className="left-[916.5px] top-[126px] absolute  w-[463.5px] h-[546px] flex flex-col gap-[18px]">
             <div>
               <h1 className="text-[15px] font-bold text-[#0FB4DB]">
                 Upgrade Freemium to 6 months
               </h1>
-              <div className="border py-[15px] px-[18px] w-[463.5px] h-[111px]  border-solid border-gray-300 justify-start">
-                <p className="font-normal text-[10.5px] leading-[16px] text-#25474F]">
-                  Agree to Wittgen’s use of sample & data information and enjoy
-                  6 months unlimited access to Wittgen’s services for free.
-                  Effective immediately.
-                </p>
-
-                <button className="mt-[27px] items-center justify-center border border-gray-300 border-solid w-[189px] h-[31px] px-[9px] py-[9px]">
-                  <span className="text-[#0FB4DB] text-[10.5px] font-semibold">
-                    Read terms & conditions
-                  </span>
-                </button>
+              <div className="mt-[9px] border  w-[463.5px] h-[111px]  border-solid border-[#CEDDE1] justify-start">
+                <div className="py-[15px] px-[18px]">
+                  <p className=" font-normal text-[10.5px] leading-[16px] text-#25474F]">
+                    Agree to Wittgen’s use of sample & data information and
+                    enjoy 6 months unlimited access to Wittgen’s services for
+                    free. Effective immediately.
+                  </p>
+                  <button className="ml-[238px] mt-[18px] items-center justify-center border border-gray-300 border-solid w-[189px] h-[31px]">
+                    <span className="text-[#0FB4DB] text-[10.5px] font-semibold">
+                      Read terms & conditions
+                    </span>
+                  </button>
+                </div>
               </div>
             </div>
 
-            <div className="relative justify-start border border-gray-300 border-solid w-[463.5px] h-[390px] ">
-              <h3 className="font-semibold text-[12px] leading-[18px] p-[25px]">
-                $100 / sample
-              </h3>
+            <div className="border border-[#CEDDE1] border-solid w-[463.5px] h-[390px] ">
+              <div className="mt-[27.5px] ml-[24px]">
+                <h3 className="font-semibold text-[12px] leading-[18px] ">
+                  $100 / sample
+                </h3>
 
-              <div className="left-[24px] absolute border border-solid border-gray-100 w-[439.5px] h-[0px] "></div>
+                <div className="border border-solid border-[#E9EBED] w-[439.5px] h-[0px] mt-[13.75px]" />
+              </div>
             </div>
+          </div>
 
-            <button className="mt-[119.5px] ml-[330px] bg-[#25474F] justify-center items-center  w-[135px] h-[36px] border border-solid border-gray-200">
+          {/* Button */}
+          <div className="absolute flex flex-row gap-[801.5px] left-[313.6px] top-[900px]">
+            <button className=" justify-center items-center  w-[135px] h-[36px] border border-solid border-[#CEDDE1]">
+              <span className="font-semibold text-[10.5px] leading-[13px] text-[#25474F]">
+                Back
+              </span>
+            </button>
+
+            <button className=" bg-[#25474F] w-[135px] h-[36px] border border-solid border-[#CEDDE1]">
               <span className="text-white font-semibold text-[10.5px] py-[18px] px-[60px] leading-[13px] ">
                 Back
               </span>
